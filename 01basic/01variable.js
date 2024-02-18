c144501b4aa8a1582ec455c2721330b2bc16efa4
shivam151 <shivamkati45@gmail.com>
2024-02-18 23:16:39 +0530

    secound commit

---
+++ 01basic/01variable.js
@@ -0,0 +1,11 @@
+
+let name = "shivam";
+const surname= "katiyar";
+var email = "shivam@gamil.com";
+console.log(name);
+console.table([email, surname, name])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
